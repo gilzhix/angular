0f609168cd7d4c5d83b5a7ffad7dce09c9ea5d36
Gilbert Stiwell Cachaya Caicedo <35930275+gilzhix@users.noreply.github.com>
2021-09-14 16:14:36 -0500

    uso de  pipes

--- platzi-store/src/app/app.module.ts
+++ platzi-store/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import {ProductComponent} from './components/product.component';
 import { CartComponent } from './cart/cart.component'
 
+
 @NgModule({
   declarations: [
     AppComponent,
