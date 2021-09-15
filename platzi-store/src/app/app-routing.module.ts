409d76d8cb411c78a258655c8a3a3e0ad66bcec2
Gilbert Stiwell Cachaya Caicedo <35930275+gilzhix@users.noreply.github.com>
2021-09-15 09:40:12 -0500

    creacion de rutas 1

--- platzi-store/src/app/app-routing.module.ts
+++ platzi-store/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {ProductComponent} from'./components/product.component'
+
+const routes: Routes = [
+  {
+    path: '/home',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
